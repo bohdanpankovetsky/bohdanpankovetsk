720e4145a28812f227e136fa9c18b0b8755ecb39
bohdanpankovetsky <33544747+bohdanpankovetsky@users.noreply.github.com>
2022-05-11 14:43:35 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,21 +1,34 @@
   "use strict";
   $(document).ready(function() {
-    var logoText = $(".header-logo h1").html();
-    var logoTextCurrent = '';
-    var counter = 0;
-    var i = setInterval(function(){
-        logoTextCurrent = logoTextCurrent + logoText.charAt(counter);
-        $(".header-logo h1").html(logoTextCurrent);
-        counter++;
-        if(counter == logoText.length) {
-            setTimeout(function() {
-            counter = 0;
-            logoTextCurrent = '';
-            $(".header-logo h1").html(logoTextCurrent);
-             setInterval(i);
-            }, 2000);
+    if($('.header-logo').length) {
+      var logoText = $(".header-logo h1").html();
+      var logoTextCurrent = '';
+      var counter = 0;
+      var i = setInterval(function(){
+          logoTextCurrent = logoTextCurrent + logoText.charAt(counter);
+          $(".header-logo h1").html(logoTextCurrent);
+          counter++;
+          if(counter == logoText.length) {
+              setTimeout(function() {
+              counter = 0;
+              logoTextCurrent = '';
+              $(".header-logo h1").html(logoTextCurrent);
+               setInterval(i);
+              }, 2000);
+          }
+      }, 100);
+      $(document).on('scroll', headerTop);
+      function headerTop() {
+        var st = $('html').scrollTop();
+        if (st >= 700) {
+          $('header').addClass('displaing');
         }
-    }, 100);
+        else {
+          $('header').removeClass('displaing');
+        }
+      }
+      headerTop();
+    }
     $('.portfolio-block').on('click', function() {
       let img = $(this).find('.portfolio-img img').attr('src');
       let title = $(this).find('.portfolio-info-title').html();
@@ -28,17 +41,7 @@
       $('.b-popup').removeClass('active');
       $('html').css('overflow-y', 'auto');
     });
-    $(document).on('scroll', headerTop);
-    function headerTop() {
-      var st = $('html').scrollTop();
-      if (st >= 700) {
-        $('header').addClass('displaing');
-      }
-      else {
-        $('header').removeClass('displaing');
-      }
-    }
-    headerTop();
+
     $('.header-menu-button').on('click', function() {
       $('.header-mob-menu').addClass('active');
       $('html').css('overflow-y', 'hidden');
